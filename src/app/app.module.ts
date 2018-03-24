96f90345c6baee7e35c44a471688f2a58c1d591b
Maksim Golitsyn <maxim@infofree.ru>
2018-03-24 20:51:43 +0500

    Render child component (server) in app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
+  // register the app component here because even throgh it's also used in bootstrap
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
   providers: [],
+  // which component should angular be aware of at the point of time the whole application starts
   bootstrap: [AppComponent]
 })
 export class AppModule { }
