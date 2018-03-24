5f6d3c321b9209eb2e0a395f3594dde7fce7c751
Maksim Golitsyn <maxim@infofree.ru>
2018-03-24 22:26:24 +0500

    Test task finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { FooterComponent } from './footer/footer.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
@@ -13,7 +16,10 @@ import { ServersComponent } from './servers/servers.component';
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    FooterComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component(<Component>{
+  selector: 'app-warning-alert',
+  template: `
+    <p>Warning! Something went wrong</p>
+  `,
+  styles: [
+    `
+      p {
+        padding: 20px;
+        background-color: mistyrose;
+        border: 1px solid red;
+      }
+  `]
+})
+export class WarningAlertComponent implements OnInit {
+
+  constructor() { }
+
+}
